8824833067a0ef04db123c8d977115bdeeed19a4
Engy Ahmed Abdelaziz <engya306@gmail.com>
2024-05-06 02:06:41 +0300

    feat: Implement product CRUD

--- src/components/Pages/AllProducts.vue
+++ src/components/Pages/AllProducts.vue
@@ -1,63 +1,76 @@
 <template>
-    <div class="container mt-4">
-        <div class="row">
-            <h3 class="display-6 text-center mt-4 mb-4">All Products</h3>
-            <div class="col-md-4" v-for="product in products" :key="product.id">
-                <div class="card">
-                    <div class="image-container">
-                        <img :src="product.thumbnail" :alt="product.title" class="card-img-top">
-                    </div>
-                    <div class="card-body">
-                        <router-link :to="'/products/' + product.id">
-                            <h5 class="card-title">{{product.title}}</h5>
-                        </router-link>
-                        <p class="card-text lead">{{product.description}}</p>
-                        <p class="card-text">${{product.price}}</p>
-                    </div>
-                    <div class="card-footer">
-                        <div class="btns">
-                            <router-link :to="'/products/edit/' + product.id" class="btn btn-primary me-3">Edit</router-link>
-                            <button class="btn btn-danger" @click="deleteProduct(product.id)">Delete</button>
-                        </div>
-                    </div>
-                </div>
+  <div class="container mt-4">
+    <div class="row">
+      <h3 class="display-6 text-center mt-4 mb-4">All Products</h3>
+      <div
+        class="col-md-4"
+        v-for="product in products"
+        :key="product.id">
+        <div class="card">
+          <div class="image-container">
+            <img
+              :src="product.thumbnail"
+              :alt="product.title"
+              class="card-img-top" />
+          </div>
+          <div class="card-body">
+            <router-link :to="'/products/' + product.id">
+              <h5 class="card-title">{{ product.title }}</h5>
+            </router-link>
+            <p class="card-text lead">{{ product.description }}</p>
+            <p class="card-text">${{ product.price }}</p>
+          </div>
+          <div class="card-footer">
+            <div class="btns">
+              <router-link
+                :to="'/products/edit/' + product.id"
+                class="btn btn-primary me-3"
+                >Edit</router-link
+              >
+              <button
+                class="btn btn-danger"
+                @click="deleteProduct(product.id)">
+                Delete
+              </button>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import getAllProducts from '../../../public/Mixins/fetchData.js'
-import deleteProductById from '../../../public/Mixins/fetchData.js'
-    export default {
-        name: 'AllProducts',
-        mixins: [getAllProducts,deleteProductById],
-        mounted() {
-            this.getAllProducts();
-        },
-        methods:{
-            deleteProduct(id){
-                console.log(id);
-                this.deleteProductById(id);
-            }
-        
-        }
-    }
+import getAllProducts from '../../../public/Mixins/fetchData.js';
+import deleteProductById from '../../../public/Mixins/fetchData.js';
+export default {
+  name: 'AllProducts',
+  mixins: [getAllProducts, deleteProductById],
+  mounted() {
+    this.getAllProducts();
+  },
+  methods: {
+    deleteProduct(id) {
+      console.log(id);
+      this.deleteProductById(id);
+    },
+  },
+};
 </script>
 
 <style scoped>
 .image-container {
-    height: 200px; 
-    overflow: hidden;
+  height: 200px;
+  overflow: hidden;
 }
 
 .card-img-top {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
-.card{
-    margin-bottom: 20px;
-    height:400px;
+.card {
+  margin-bottom: 20px;
+  height: 400px;
 }
 </style>
--- src/components/Pages/productDetails.vue
+++ src/components/Pages/productDetails.vue
@@ -1,55 +1,60 @@
 <template>
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-lg-6 col-12">
-                <div class="image-container">
-                    <img :alt="ProductDetails.title" :src="ProductDetails.thumbnail">
-                </div>
-            </div>
-            <div class="col-lg-6 col-12">
-                <h3 class="display-6">{{ProductDetails.title}}</h3>
-                <p class="lead">{{ProductDetails.description}}</p>
-                <p>Price: $ {{ProductDetails.price}}</p>
-                <div class="btns">
-                    <router-link :to="'/products/edit/' + ProductDetails.id" class="btn btn-primary me-3">Edit</router-link>
-                    <button class="btn btn-danger" @click="deleteProduct(ProductDetails.id)">Delete</button>
-                </div>
-            </div>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-lg-6 col-12">
+        <div class="image-container">
+          <img
+            :alt="ProductDetails.title"
+            :src="ProductDetails.thumbnail" />
         </div>
+      </div>
+      <div class="col-lg-6 col-12">
+        <h3 class="display-6">{{ ProductDetails.title }}</h3>
+        <p class="lead">{{ ProductDetails.description }}</p>
+        <p>Price: $ {{ ProductDetails.price }}</p>
+        <div class="btns">
+          <router-link
+            :to="'/products/edit/' + ProductDetails.id"
+            class="btn btn-primary me-3"
+            >Edit</router-link
+          >
+          <button
+            class="btn btn-danger"
+            @click="deleteProduct(ProductDetails.id)">
+            Delete
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import getProductById from '../../../public/Mixins/fetchData.js'
-import deleteProductById from '../../../public/Mixins/fetchData.js'
+import getProductById from '../../../public/Mixins/fetchData.js';
+import deleteProductById from '../../../public/Mixins/fetchData.js';
 
-
-    export default {
-        name: 'ProductDetails',
-        data(){
-            return {
-                id: '',
-            }
-        
-        },
-        mixins: [getProductById,deleteProductById],
-        created(){
-            this.id = this.$route.params.id;
-        },
-        mounted() {
-            this.getProductById(this.id);
-        },
-        methods:{
-            deleteProduct(id){
-                console.log(id);
-                this.deleteProductById(id);
-                this.$router.push('/products');
-            }
-        }
-        
-    }
+export default {
+  name: 'ProductDetails',
+  data() {
+    return {
+      id: '',
+    };
+  },
+  mixins: [getProductById, deleteProductById],
+  created() {
+    this.id = this.$route.params.id;
+  },
+  mounted() {
+    this.getProductById(this.id);
+  },
+  methods: {
+    deleteProduct(id) {
+      console.log(id);
+      this.deleteProductById(id);
+      this.$router.push('/products');
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
